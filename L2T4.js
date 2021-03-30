7154f350e86dc1fe633f42b30aaac39619ebaea8
KindFest <kindfey@ya.ru>
2021-03-30 08:37:22 +0300

    Lesson 2

---
+++ L2T4.js
@@ -0,0 +1,22 @@
+"use strict";
+var a = 0;
+var b = '';
+switch (a) {
+    case 0: b += a++ + ' ';
+    case 1: b += a++ + ' ';
+    case 2: b += a++ + ' ';
+    case 3: b += a++ + ' ';
+    case 4: b += a++ + ' ';
+    case 5: b += a++ + ' ';
+    case 6: b += a++ + ' ';
+    case 7: b += a++ + ' ';
+    case 8: b += a++ + ' ';
+    case 9: b += a++ + ' ';
+    case 10: b += a++ + ' ';
+    case 11: b += a++ + ' ';
+    case 12: b += a++ + ' ';
+    case 13: b += a++ + ' ';
+    case 14: b += a++ + ' ';
+    case 15: b += a++ + ' ';
+}
+alert(b);
---
+++ L2T5.js
@@ -0,0 +1,21 @@
+"use strict";
+function sumNumbers(a, b) {
+    return a + b;
+}
+
+function difNumbers(a, b) {
+    return a - b;
+}
+
+function mulNumbers(a, b) {
+    return a * b;
+}
+
+function divNumbers(a, b) {
+    return a / b;
+}
+
+alert(sumNumbers(5, 2));
+alert(difNumbers(5, 2));
+alert(mulNumbers(5, 2));
+alert(divNumbers(5, 2));
